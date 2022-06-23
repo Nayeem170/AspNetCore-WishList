a0c6d22a64412459a067c9e98a1b7786af37d32b
Nayeem Bin Ahsan <nayeemasis@yahoo.com>
2022-06-24 00:21:42 +0600

    Completed module

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WishList.Models;
 
 namespace WishList.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(ApplicationDbContext options) : base(options)
+        public DbSet<Item> Items { get; set; }
+        public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
     }
